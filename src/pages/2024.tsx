442c2130ceab94e2f38c4f46ce2490b5d6b484df
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-12-29 03:54:17 +0100

    create Home and 2024 components

---
+++ src/pages/2024.tsx
@@ -0,0 +1,7 @@
+export function NextYear() {
+  return (
+    <div>
+      <p style={{ fontSize: '2rem', fontWeight: 'bold' }}>Welcome to 2024!!!🎉🍾🎊</p>
+    </div>
+  );
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+export function Home() {
+  return (
+    <div>
+      <p style={{ fontSize: '2rem', fontWeight: 'bold' }}>Adios 2023, thanks for a great time!🫡</p>
+    </div>
+  );
+}
